fe1aa12991cc980bdfceaa8c40fbcd2c94167a6b
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2017-06-13 23:32:51 -0700

    Put in includes and namespaces, main function.

--- detect_face_and_crop.cpp
+++ detect_face_and_crop.cpp
@@ -6,4 +6,17 @@
 //
 //
 
-#include <stdio.h>
+#include <dlib/dnn.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/clustering.h>
+#include <dlib/string.h>
+#include <dlib/image_io.h>
+#include <dlib/image_processing/frontal_face_detector.h>
+
+using namespace dlib;
+using namespace std;
+
+int main (int argc, char** argv) try
+{
+    
+}
