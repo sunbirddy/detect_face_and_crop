11f954d515c56f9bb614b3a167b2cb50f2c8fdd2
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2017-06-20 22:58:01 +0800

    Using regular face detection instead.

--- detect_face_and_crop.cpp
+++ detect_face_and_crop.cpp
@@ -6,80 +6,51 @@
 //
 //
 
-#include <dlib/dnn.h>
-#include <dlib/gui_widgets.h>
-#include <dlib/clustering.h>
-#include <dlib/string.h>
-#include <dlib/image_io.h>
 #include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/gui_widgets.h>
+#include <dlib/image_io.h>
+#include <iostream>
 
 using namespace dlib;
 using namespace std;
 
-template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
-using residual = add_prev1<block<N,BN,1,tag1<SUBNET>>>;
-
-template <template <int,template<typename>class,int,typename> class block, int N, template<typename>class BN, typename SUBNET>
-using residual_down = add_prev2<avg_pool<2,2,2,2,skip1<tag2<block<N,BN,2,tag1<SUBNET>>>>>>;
-
-template <int N, template <typename> class BN, int stride, typename SUBNET>
-using block  = BN<con<N,3,3,1,1,relu<BN<con<N,3,3,stride,stride,SUBNET>>>>>;
-
-template <int N, typename SUBNET> using ares      = relu<residual<block,N,affine,SUBNET>>;
-template <int N, typename SUBNET> using ares_down = relu<residual_down<block,N,affine,SUBNET>>;
-
-template <typename SUBNET> using alevel0 = ares_down<256,SUBNET>;
-template <typename SUBNET> using alevel1 = ares<256,ares<256,ares_down<256,SUBNET>>>;
-template <typename SUBNET> using alevel2 = ares<128,ares<128,ares_down<128,SUBNET>>>;
-template <typename SUBNET> using alevel3 = ares<64,ares<64,ares<64,ares_down<64,SUBNET>>>>;
-template <typename SUBNET> using alevel4 = ares<32,ares<32,ares<32,SUBNET>>>;
-
-using anet_type = loss_metric<fc_no_bias<128,avg_pool_everything<
-                            alevel0<
-                            alevel1<
-                            alevel2<
-                            alevel3<
-                            alevel4<
-                            max_pool<3,3,2,2,relu<affine<con<32,7,7,2,2,
-                            input_rgb_image_sized<150>
-                            >>>>>>>>>>>>;
-
-int main (int argc, char** argv) try
+int main(int argc, char** argv)
 {
-    /* from dnn face recognition example */
-    if (argc != 2)
+    try
     {
-        cout << "Run this example by invoking it like this: " << endl;
-        cout << "   ./dnn_face_recognition_ex faces/bald_guys.jpg" << endl;
-        cout << endl;
-        cout << "You will also need to get the face landmarking model file as well as " << endl;
-        cout << "the face recognition model file.  Download and then decompress these files from: " << endl;
-        cout << "http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
-        cout << "http://dlib.net/files/dlib_face_recognition_resnet_model_v1.dat.bz2" << endl;
-        cout << endl;
-        return 1;
+        if (argc == 1)
+        {
+            std::cout << "Give some image files as arguments to this program." << std::endl;
+            return 0;
+        }
+        
+        frontal_face_detector = get_frontal_face_detector();
+        image_window win;
+        
+        for (int i = 1; i < argc; ++i)
+        {
+            std::cout << "processing image " << argv[i] << std::endl;
+            array2d<unsigned char> img;
+            load_image(img, argv[i]);
+            
+            pyramid_up(img);
+            
+            std::vector<rectangle> dets = detector(img);
+            
+            std::cout << "Number of faces detected: " << dets.sie() << std::endl;
+            
+            win.clear_overlay();
+            win.set_image(img);
+            win.add_overlay(dets, rgb_pixel(255, 0, 0));
+            draw_rectangle(img, )
+            
+            std::cout << "Hit enter to process the next image..." << std::endl;
+            cin.get();
+        }
     }
-        
-    frontal_face_detector detector = get_frontal_face_detector();
-    shape_predictor sp;
-    deserialize("shape_predictor_68_face_landmarks.dat") >> sp;
-    anet_type net;
-    deserialize("dlib_face_recognition_resnet_model_v1.dat") >> net;
-        
-    matrix<rgb_pixel>> faces;
-    for (auto face: detector(img))
+    catch (exception& e)
     {
-        auto shape = sp(img, face);
-        matrix<rgb_pixel> face_chip;
-        extract_image_chip(img, get_face_chip_details(shape,150,0.25), face_chip);
-        faces.push_back(move(face)chip));
-        win.add_verlay(face);
+        std::cout << "\nexception thrown!" << std::endl;
+        std::cout << e.what() << std::endl;
     }
-        
-    if (faces.size() == 0)
-    {
-        std::cout << "No faces found in image!" << std::endl;
-        return 1;
-    }
-        
 }
